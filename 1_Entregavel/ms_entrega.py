f2a64f4f5f3bec5264589a995e8007f538246d37
RudineiKuznier <rudineikuzniersilva@gmail.com>
2026-09-17 00:57:57 -0300

    (0,1) Microsserviço Entrega

---
+++ 1_Entregavel/ms_entrega.py
@@ -0,0 +1,72 @@
+import pika
+import json
+import time 
+import random
+
+def enviar_evento_entrega(channel, routing_key, dados_evento):
+    message = json.dumps(dados_evento)
+    channel.basic_publish(
+        exchange='eCommerce',
+        routing_key=routing_key,
+        body=message,
+        properties=pika.BasicProperties(
+            content_type='application/json',
+            delivery_mode=pika.DeliveryMode.Persistent
+        )
+    )
+    print(f"    Enviado para eCommerce {routing_key}: {message}")
+
+def processar_entrega(channel, id_pedido):
+
+    numero_nf = f"NF-{random.randint(10000, 99999)}"
+    codigo_rastreio = f"BR{random.randint(100000000, 999999999)}BR"
+
+    message = {
+        "id_pedido": id_pedido,
+        "status": "PEDIDO_ENVIADO",
+        "numero_nota_fiscal": numero_nf,
+        "codigo_rastreio": codigo_rastreio
+    }
+    time.sleep(3.0)
+
+    enviar_evento_entrega(channel, 'pedido.enviado', message)
+     
+    
+def iniciar_ms_entrega():
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host='localhost', virtual_host='my_vhost'))
+    channel = connection.channel()
+
+    channel.exchange_declare(exchange='eCommerce', exchange_type='direct')
+
+    queue_name = 'ms_fila_entrega'
+
+    channel.queue_declare(queue=queue_name, durable=True)
+
+    channel.queue_bind(exchange='eCommerce', queue=queue_name, routing_key='pagamento.aprovado')
+
+    print(' [*] Aguardando eventos de entrega. Para sair pressione CTRL+C')
+
+    channel.basic_qos(prefetch_count=1)
+    channel.basic_consume(
+        queue=queue_name, on_message_callback=callback, auto_ack=False)
+
+    channel.start_consuming()
+
+        
+def callback(ch, method, properties, body):
+    if (method.routing_key == 'pagamento.aprovado'):
+        dados = json.loads(body.decode('utf-8'))
+        id_pedido = dados.get('id_pedido')
+        print("-" * 40);
+        print(f"Routing Key: {method.routing_key}")
+        print(f"ID do Pedido: {id_pedido}")
+                
+        processar_entrega(ch, id_pedido)
+    
+        print("-" * 40)
+    
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+        
+if __name__ == '__main__':
+    iniciar_ms_entrega()
