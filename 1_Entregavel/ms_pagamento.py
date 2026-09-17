a38d241756822762ec4c4db89d78206407861ee2
RudineiKuznier <rudineikuzniersilva@gmail.com>
2026-09-17 00:57:08 -0300

    (0,2) Microsservico Pagamento

---
+++ 1_Entregavel/ms_pagamento.py
@@ -0,0 +1,72 @@
+import pika
+import json
+import random
+
+def enviar_evento_pagamento(channel, routing_key, dados_evento):
+    message = json.dumps(dados_evento)
+    channel.basic_publish(
+        exchange='eCommerce',
+        routing_key=routing_key,
+        body=message,
+        properties=pika.BasicProperties(
+            content_type='application/json',
+            delivery_mode=pika.DeliveryMode.Persistent
+        )
+    )
+    print(f"    Enviado para eCommerce {routing_key}: {message}")
+
+def processar_pagamento(channel, id_pedido):
+    aprovado = random.choice([True, False])
+
+    if (aprovado):
+        message = {
+            "id_pedido": id_pedido,
+            "status": "PAGAMENTO_APROVADO"
+        }
+        enviar_evento_pagamento(channel, 'pagamento.aprovado', message)
+    else:
+        message = {
+            "id_pedido": id_pedido, 
+            "status": "PAGAMENTO_RECUSADO", 
+            "motivo": "Saldo insuficiente"
+        }
+        enviar_evento_pagamento(channel, 'pagamento.recusado', message)    
+    
+def iniciar_ms_pagamento():
+    connection = pika.BlockingConnection(
+        pika.ConnectionParameters(host='localhost', virtual_host='my_vhost'))
+    channel = connection.channel()
+
+    channel.exchange_declare(exchange='eCommerce', exchange_type='direct')
+
+    queue_name = 'ms_fila_pagamento'
+
+    channel.queue_declare(queue=queue_name, durable=True)
+
+    channel.queue_bind(exchange='eCommerce', queue=queue_name, routing_key='pedido.estoque_ok')
+
+    print(' [*] Aguardando eventos de pagamento. Para sair pressione CTRL+C')
+
+    channel.basic_qos(prefetch_count=1)
+    channel.basic_consume(
+        queue=queue_name, on_message_callback=callback, auto_ack=False)
+
+    channel.start_consuming()
+
+        
+def callback(ch, method, properties, body):
+    if (method.routing_key == 'pedido.estoque_ok'):
+        dados = json.loads(body.decode('utf-8'))
+        id_pedido = dados.get('id_pedido')
+        print("-" * 40);
+        print(f"Routing Key: {method.routing_key}")
+        print(f"ID do Pedido: {id_pedido}")
+                
+        processar_pagamento(ch, id_pedido)
+    
+        print("-" * 40)
+    
+    ch.basic_ack(delivery_tag=method.delivery_tag)
+        
+if __name__ == '__main__':
+    iniciar_ms_pagamento()
